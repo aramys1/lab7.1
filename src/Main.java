d4090e0c593750b3182a952f987add092718115d
aramys1 <aramysc790@gmail.com>
2025-06-19 08:53:53 -0500

    case del main basico

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
         //hola mundo
         //prueba en intellij
+
+        BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+
+        int accesoCase = 0;
+
+        try{
+            System.out.println("1. Problema1\n2.Problema2\n3.Problema3\n4.Problema4");
+            accesoCase = Integer.parseInt(bufer.readLine());
+
+            switch (accesoCase){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        catch (NumberFormatException e){
+            System.out.println("formato de numero equivocado");
+        }
+        catch (IOException e){
+            System.out.println("Io error");
+        }
+
     }
 }
--- src/Problema3.java
+++ src/Problema3.java
@@ -1,2 +1,18 @@
 public class Problema3 {
+
+
+    int[][] enteros = {
+        {1,2,3},
+        {3,4,5},
+        {6,7,8},
+    };
+
+    void generarVector(int enteros[][]){
+
+    }
+
+
+
 }
+
+
