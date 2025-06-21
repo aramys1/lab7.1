467f35bccd92132d62d145d08b4d473aa2f6c064
Diego <163057208+Saintdiegx@users.noreply.github.com>
2025-06-21 16:57:46 -0500

    Main del problema 4 pero en comentario

--- src/Problema4.java
+++ src/Problema4.java
@@ -1,2 +1,38 @@
 public class Problema4 {
 }
+/*
+int cantidadEstudiantes;
+int i = 0;
+ try{
+        System.out.println("Ingrese la cantidad de estudiantes: ");
+        cantidadEstudiantes = Integer.parseInt(bufer.readLine());
+
+    }catch(IOException e){
+        System.out.println("\nError al leer la entrada:\n");
+        continue;
+    }catch(NumberFormatException e){
+        System.out.println("\nError en el formato de entrada\n");
+        continue; 
+    }
+
+int []notas = new int[cantidadEstudiantes];
+String []nombres = new String[cantidadEstudiantes];
+
+while(i < cantidadEstudiantes){
+    try{
+        System.out.println("Ingrese el nombre del estudiante " + (i + 1) + ": ");
+        nombres[i] = bufer.readLine();
+        System.out.println("Ingrese la nota del estudiante " + (i + 1) + ": ");
+        notas[i] = Integer.parseInt(bufer.readLine());
+    }catch(IOException e){
+        System.out.println("\nError al leer la entrada:\n");
+        continue;
+    }catch(NumberFormatException e){
+        System.out.println("\nError en el formato de entrada\n");
+        continue; 
+    }
+    i++;
+}
+
+
+ */
